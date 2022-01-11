bb28c38c9b8932912bf9d092085d4ecec6c892a2
dns <pochti_zvezda@mail.ru>
2022-01-11 23:06:28 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/onlineshopping/OnlineShoppingApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.onlineshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShoppingApplication.class, args);
+    }
+
+}
